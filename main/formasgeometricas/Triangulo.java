70d0a71007a794ed9fbf68109e82553ad523d314
mariane-firmino <marianecfm2024@gmail.com>
2025-11-07 11:27:32 -0400

    Update Triangulo.java

--- main/formasgeometricas/Triangulo.java
+++ main/formasgeometricas/Triangulo.java
@@ -2,28 +2,19 @@ package main.formasgeometricas;
 
 public class Triangulo {
     double area;
-    double lado1, lado2, lado3;
     double base;
     double altura;
-    double perimetro;
 
-    Triangulo (double base, double lado1, double lado2, double lado3, double altura) {
+    Triangulo (double base,  double altura) {
         this.base = base;
-        this.lado1 = lado1;
-        this.lado2 = lado2;
-        this.lado3 = lado3;
         this.altura = altura;
         this.area = (base * altura) / 2;
-        this.perimetro = lado1 + lado2 + lado3;
     }
 
     void imprimir () {
         System.out.println("Base: " + base);
-        System.out.println("Lado 1: " + lado1);
-        System.out.println("Lado 2: " + lado2);
-        System.out.println("Lado 3: " + lado3);
         System.out.println("Altura: " + altura);
         System.out.println("Área: " + area);
-        System.out.println("Perímetro: " + perimetro);
     }
 }
+
